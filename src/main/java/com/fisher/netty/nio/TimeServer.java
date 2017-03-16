b8e71d5c6f32cac2844a2b22a14a05ab867a7e9f
fisbii <fisher.bi@leandev.se>
2017-03-16 17:15:05 +0800

    netty time demo.

---
+++ src/main/java/com/fisher/netty/nio/TimeServer.java
@@ -0,0 +1,45 @@
+package com.fisher.netty.nio;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * Created by fisbii on 17-3-16.
+ */
+public class TimeServer {
+    public void bind(int port) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup,workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .childHandler(new ChildChannelHandler());
+            ChannelFuture f = b.bind(port).sync();
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    private class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+        @Override
+        protected void initChannel(SocketChannel socketChannel) throws Exception {
+            socketChannel.pipeline().addLast(new TimeServerHandler());
+        }
+    }
+
+    public static void main(String[] args){
+        int port = 8081;
+        if(args != null && args.length > 0){
+            port = Integer.valueOf(args[0]);
+        }
+        new TimeServer().bind(port);
+    }
+}
